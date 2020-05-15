5bab78c247776e35278eefc2ba521aae528e2e61
w-xuefeng <xuefeng@wangxuefeng.com.cn>
2020-05-15 15:04:58 +0800

    fix: fixed types

--- global.d.ts
+++ global.d.ts
@@ -1,9 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare const api: any
-declare const $api: any
-declare let apiready: () => any
 
 interface Window {
   api: any;
-  $api: any;
-  apiready: () => any;
 }
